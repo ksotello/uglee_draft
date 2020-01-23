0b10c758bb2d928979ccacc3d3a11735623a2ef2
Kyle Sotello <kylesotello@Gmail.com>
2020-01-23 08:56:37 -0500

    port update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const nodemailer = require('nodemailer');
 const app = express()
 const cors = require('cors')
-const port = 3000
+const port = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors())
